90760e9d5f06498f5fb2c1f408c1dabf578ca24b
Meng <gosuxm@gmail.com>
2024-04-25 11:19:30 +0800

    ui: add language switch link

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,4 +1,5 @@
 import { getDictionary } from "./dictionaries";
+import Link from "next/link";
 
 export async function generateMetadata({
   params: { lang },
@@ -21,6 +22,11 @@ export default async function Home({
   const t = await getDictionary(lang);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="space-x-2">
+        <Link href="/en">English</Link>
+        <span>|</span>
+        <Link href="/zh">Chinese/中文</Link>
+      </div>
       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         {t.home.title}
       </p>
